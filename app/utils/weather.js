d6dfea8c20d19c8f30ff43ad8330b6a39945c1d9
DataNick <DataNick@users.noreply.github.com>
2016-11-18 01:47:05 -0500

    Create weather helper to retrieve weather data

---
+++ app/utils/weather.js
@@ -0,0 +1,29 @@
+var axios = require('axios');
+
+var BASEURL = "http://api.openweathermap.org/data/2.5/";
+var WEATHER = "weather?q=CITY-NAME&type=accurate&APPID=" + APIKEY;
+var FORECAST = "forecast/daily?q=CITY-NAME&type=accurate&APPID=" + APIKEY + "&cnt=5";
+var APIKEY = '9cecc6b72c2a20f9f6ac913d1daf6fd8'
+
+function getCityWeather(cityname) {
+  return axios.get(BASEURL + "weather?q=" + cityname + "&type=accurate&APPID=9cecc6b72c2a20f9f6ac913d1daf6fd8").then(function (currentWeatherData) {
+      console.log(currentWeatherData.data)
+    }).catch(function(error){
+      console.log(error)
+    })
+  }
+
+
+function getCityForecast(cityname) {
+  return axios.get(BASEURL + "forecast/daily?q=" + cityname + "&type=accurate&APPID=9cecc6b72c2a20f9f6ac913d1daf6fd8&cnt=5")
+  .then(function(currentForecastData){
+    console.log(currentForecastData.data)
+  })
+}
+
+var helpers = {
+  getCityWeather: getCityWeather,
+  getCityForecast: getCityForecast
+};
+
+module.exports = helpers;
