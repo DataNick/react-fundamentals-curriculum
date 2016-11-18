521dd1acbec33f63df1456980cdd5634dc1b2d82
DataNick <DataNick@users.noreply.github.com>
2016-11-18 17:51:02 -0500

    Add new forecast route

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,14 +6,16 @@ var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 
-var Main = require('../components/Main');
-var Home = require('../components/Home');
+var Main = require('../containers/Main');
+var HomeContainer = require('../containers/HomeContainer');
+var ForecastContainer = require('../containers/ForecastContainer');
+
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main} >
-    <IndexRoute component={Home} />
-
+      <IndexRoute component={HomeContainer} />
+      <Route path="forecast/:city" component={ForecastContainer} />
     </Route>
   </Router>
   )
